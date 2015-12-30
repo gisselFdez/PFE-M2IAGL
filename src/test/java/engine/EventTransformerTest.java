d0ec09f4ef22b82aab6737b32920669b5b075cb4
giss04 <gisselta04@hotmail.com>
2015-12-30 18:16:47 +0100

    test: adding missing test

---
+++ src/test/java/engine/EventTransformerTest.java
@@ -0,0 +1,37 @@
+package engine;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Test;
+
+public class EventTransformerTest {
+
+	@Test
+	public void testGetRobotiumMethods() {		
+		//Test data
+		List<HashMap<String,String>> androidEvents = new ArrayList<HashMap<String,String>>();
+		HashMap<String,String> map = new HashMap<String,String>();
+		map.put("mText", "OK");
+		map.put("action", "onClick");
+		map.put("instanceOf", "Button");
+		androidEvents.add(map);
+		
+		//expected result
+		List<String> expected = Arrays.asList("clickOnButton(\"OK\")");
+		
+		//Tested class
+		EventTransformer event = new EventTransformer();
+		List<String> robotiumEvents = event.getRobotiumMethods(androidEvents);
+		
+		assertTrue("Expected 'robotiumEvents' and 'expected' to be equal."+
+	            "\n  'robotiumEvents' = "+robotiumEvents+
+	            "\n  'expected' = "+expected, 
+	            expected.equals(robotiumEvents));
+	}
+
+}
